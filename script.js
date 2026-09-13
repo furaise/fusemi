190b00217de4071a3457e723a665cbdfb2d0248a
chatbots <furaiseminerals@gmail.com>
2026-09-13 11:50:56 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+document.querySelector('.menu').addEventListener('click',()=>document.querySelector('nav').classList.toggle('show'));
+document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>document.querySelector('nav').classList.remove('show')));
